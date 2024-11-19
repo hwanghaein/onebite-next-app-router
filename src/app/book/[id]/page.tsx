72d0ff3a623dfc10d7ce7efe2850e9d8942d5a77
황혜인 <hhi9037@naver.com>
2024-11-19 22:56:42 +0900

    서버 액션

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -2,19 +2,15 @@ import { notFound } from "next/navigation";
 import style from "./page.module.css";
 
 
-export function generateStaticParams(){
-  return [{id:"1"},{id:"2"},{id:"3"},];
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" },];
 }
 
-export default async function Page({
-  params,
-}: {
-  params: { id: string | string[] };
-}) {
+async function BookDetail({ bookId }: { bookId: string }) {
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`)
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${bookId}`)
   if (!response.ok) {
-    if(response.status===404){
+    if (response.status === 404) {
       notFound();
     }
     return <div>오류가 발생했습니다 ...</div>
@@ -32,7 +28,7 @@ export default async function Page({
   } = book;
 
   return (
-    <div className={style.container}>
+    <section className={style.container}>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${coverImgUrl}')` }}
@@ -45,6 +41,37 @@ export default async function Page({
         {author} | {publisher}
       </div>
       <div className={style.description}>{description}</div>
-    </div>
+    </section>
   );
+}
+
+function ReviewEditor() {
+
+  async function createReviewAction(formData: FormData) {
+    "use server";
+    const content= formData.get('content')?.toString();
+    const author= formData.get('author')?.toString();
+    console.log(content, author);
+    
+    
+  }
+
+  return <section>
+    <form action={createReviewAction}>
+      <input name="content" placeholder="리뷰내용" />
+      <input name="author" placeholder="작성자" />
+      <button type="submit">작성하기</button>
+    </form>
+  </section>
+}
+
+export default function Page({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return <div className={style.container}>
+    <BookDetail bookId={params.id} />
+    <ReviewEditor />
+  </div>
 }
