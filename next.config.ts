bf83cb4190ebcb799d111ed091065d714e1d0614
황혜인 <hhi9037@naver.com>
2024-11-21 15:06:56 +0900

    이미지 최적화

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     // 빌드 시 타입 오류 무시
     ignoreBuildErrors: true,
   },
+  images :{
+    domains : ["shopping-phinf.pstatic.net"],
+  }
 };
 
 export default nextConfig;
