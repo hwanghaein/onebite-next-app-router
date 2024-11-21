5b59ac1035ed2c528577326249903154f238c65e
황혜인 <hhi9037@naver.com>
2024-11-21 15:06:37 +0900

    parallel & intercepting 라우트

--- src/app/@modal/(.)book/[id]/page.tsx
+++ src/app/@modal/(.)book/[id]/page.tsx
@@ -3,5 +3,4 @@ import BookPage from "@/app/book/[id]/page"
 import Modal from "@/components/modal"
 
 export default function Page(props: any) {
-  return <div>가로채기 성공! <Modal><BookPage {...props} /></Modal></div>
-}
+  return  <Modal><BookPage {...props} /></Modal>}
